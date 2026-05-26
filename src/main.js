cc84a6c996ffa87d305be7bc1d429b99582c8e8c
Philipp Fromme <philipp.fromme@camunda.com>
2026-05-26 18:13:42 +0200

    chore: update about

--- src/main.js
+++ src/main.js
@@ -30,6 +30,7 @@ document.querySelector('#app').innerHTML = `
         <li>BPMN rendering improvements based on the following principles:</li>
         <ul>
           <li>Refreshed BPMN symbols for cleaner visuals.</li>
+          <li>Show outline on hover</li>
           <li>Rounded corners for friendlier, more modern look.</li>
         </ul>
       </ul>
